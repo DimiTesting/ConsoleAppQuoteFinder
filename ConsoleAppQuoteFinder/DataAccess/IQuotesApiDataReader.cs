c092544360b559631379440b0ff03d33a438f4c1
amankeldi.kurban <dimitesting31@gmail.com>
2025-05-11 23:05:56 +0300

    Add project files.

---
+++ ConsoleAppQuoteFinder/DataAccess/IQuotesApiDataReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppQuoteFinder.DataAccess
+{
+    public interface IQuotesApiDataReader
+    {
+        public Task<string> ReadAsync(int page, int quotesPerPage);
+    }
+}
---
+++ ConsoleAppQuoteFinder/Models/Datum.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppQuoteFinder.Models
+{
+    public class Datum
+    {
+        public string _id { get; set; }
+        public string quoteText { get; set; }
+        public string quoteAuthor { get; set; }
+        public string quoteGenre { get; set; }
+        public int __v { get; set; }
+    }
+}
