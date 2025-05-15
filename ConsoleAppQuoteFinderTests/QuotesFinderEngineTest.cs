a5650fb783fd36e7e671bba58750daf08e8f7798
amankeldi.kurban <dimitesting31@gmail.com>
2025-05-15 20:06:59 +0300

    Testing with NUnit, quotesFinderEngine and quotesToStringConverter classes

---
+++ ConsoleAppQuoteFinderTests/QuotesFinderEngineTest.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleAppQuoteFinder.DataProcessor;
+using NUnit.Framework;
+
+namespace ConsoleAppQuoteFinderTests
+{
+    public class QuotesFinderEngineTest
+    {
+        [TestCase("Catch me outside how about that", "that", "Catch me outside how about that")]
+        [TestCase("Wiggle wiggle you got to see the eagle", "got", "Wiggle wiggle you got to see the eagle")]
+        [TestCase("It is my liiifeeee", "me", "No quotes found")]
+        public void DisplayQuoteIfWordIsInIt(string quotes, string word, string expected)
+        {
+            var quotesFinderEngine = new QuoteFinderEngine();
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                quotesFinderEngine.DisplayFoundQuotes(quotes, word);
+
+                var result = sw.ToString().Trim();
+
+                Assert.AreEqual(result, expected);
+            }
+        }
+    }
+}
---
+++ ConsoleAppQuoteFinderTests/QuotesToStringCoverterTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using ConsoleAppQuoteFinder.DataProcessor;
+
+namespace ConsoleAppQuoteFinderTests
+{
+    public class QuotesToStringCoverterTest
+    {
+        [Test]
+        public void ConvertShallReturnMultilineStringFromJsonContainingKeysDataAndQuoteText()
+        {
+			var converter = new QuotesToStringConverter();
+
+			var input = @"{
+			  ""statusCode"": 200, 
+			  ""message"": ""success"", 
+			  ""pagination"" :  {
+						""page"": 1
+			  },
+			  ""totalQuotes"": 10, 
+			  ""data"" : 
+				[
+				 {
+				   ""_id"" : ""1"", 
+				   ""quoteText"": ""Mock1 text 1"",
+				   ""quoteAuthor"": ""Mock"", 
+				   ""quoteGenre"": ""Mock"", 
+				   ""_v"": 1
+				 },
+				 {
+				   ""_id"" : ""2"", 
+				   ""quoteText"": ""Mock2 text 2"",
+				   ""quoteAuthor"": ""Mock"", 
+				   ""quoteGenre"": ""Mock"", 
+				   ""_v"": 2
+				 }
+				]
+			}";
+
+            var result = converter.Convert(input);
+
+            var expected = $"Mock1 text 1{Environment.NewLine}Mock2 text 2";
+
+            Assert.AreEqual(expected, result, "Json file should be formated as defined in Models folder");
+        }
+
+		[Test]
+		public void ConvertShallThrowExceptionFromJsonNotContainingKeysDataAndQuoteText()
+		{
+            var converter = new QuotesToStringConverter();
+
+			var input = "Not correctly formatted";
+
+			Assert.Throws<System.Text.Json.JsonException>(
+				() => converter.Convert(input), "Json file should be formated as defined in Models folder");
+        }
+    }
+}
